f249ec8c8bcc056e3cb017c095826099fb3bcbe6
travelingdeveloper12 <dutta_ari@yahoo.in>
2021-04-17 14:28:55 +0100

    GET, UPDATE, and DELETE

--- src/main/java/com/musicreview/controller/SongsController.java
+++ src/main/java/com/musicreview/controller/SongsController.java
@@ -2,13 +2,13 @@ package com.musicreview.controller;
 
 import com.musicreview.dto.SongsDTO;
 import com.musicreview.helper.MessageResponse;
+import com.musicreview.service.SongsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.musicreview.service.SongsService;
 
 @RestController
 @RequestMapping("/songs")
@@ -20,15 +20,56 @@ public class SongsController {
     private SongsService songsService;
 
     @GetMapping("/")
-    public String checkController(){
-        return "The controller is reachable";
+    public ResponseEntity<?> get() {
+        logger.info("Songs Controller : {GET ALL}");
+        return new ResponseEntity<>(songsService.get(), HttpStatus.OK);
     }
+
     @PostMapping("/add")
     public ResponseEntity<?> add(@RequestBody SongsDTO songsDTO) {
-        logger.info("Songs Controller : {}", songsDTO.toString());
+        logger.info("Songs Controller : {POST}", songsDTO.toString());
         songsService.save(songsDTO);
         MessageResponse messageResponse = new MessageResponse();
         messageResponse.setMessage("Song Creation Successful. Created on : " + System.currentTimeMillis());
         return new ResponseEntity<>(messageResponse.getMessage(), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") String id, @RequestBody SongsDTO songsDTO) {
+        logger.info("Songs Controller : {PUT}", songsDTO.toString());
+        MessageResponse messageResponse = new MessageResponse();
+        if (songsService.updateById(id, songsDTO)) {
+            messageResponse.setMessage("Song Update Successful. Updated on : " + System.currentTimeMillis());
+            return new ResponseEntity<>(messageResponse.getMessage(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(messageResponse.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/tutorials/{id}")
+    public ResponseEntity<?> deleteTutorial(@PathVariable("id") String id) {
+        logger.info("Songs Controller : {DELETE BY ID}", id);
+        MessageResponse messageResponse = new MessageResponse();
+        if (songsService.deleteById(id)) {
+            messageResponse.setMessage("Song Delete By Id Successful. Deleted on : " + System.currentTimeMillis());
+            return new ResponseEntity<>(messageResponse.getMessage(), HttpStatus.OK);
+        } else {
+            messageResponse.setMessage("No Songs Available");
+            return new ResponseEntity<>(messageResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/delete/songs")
+    public ResponseEntity<?> deleteAllTutorials() {
+        logger.info("Songs Controller : {DELETE ALL}");
+        MessageResponse messageResponse = new MessageResponse();
+        if (songsService.deleteAll()) {
+            messageResponse.setMessage("ALL Songs Delete Successful. Deleted on : " + System.currentTimeMillis());
+            return new ResponseEntity<>(messageResponse, HttpStatus.NO_CONTENT);
+        } else {
+            messageResponse.setMessage("No Songs Available");
+            return new ResponseEntity<>(messageResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 }
--- src/main/java/com/musicreview/service/SongsService.java
+++ src/main/java/com/musicreview/service/SongsService.java
@@ -2,11 +2,14 @@ package com.musicreview.service;
 
 import com.musicreview.dto.SongsDTO;
 import com.musicreview.entity.Songs;
+import com.musicreview.repository.SongsRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.musicreview.repository.SongsRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SongsService {
@@ -17,9 +20,47 @@ public class SongsService {
     private SongsRepository songsRepository;
 
     public void save(SongsDTO songsDTO) {
-        logger.info("Inside SongsService layer!!");
+        logger.info("Inside SongsService layer!! - [Create Song]");
         Songs songs = new Songs(songsDTO.getName(), songsDTO.getArtistName(), songsDTO.getCountryArea());
         songsRepository.save(songs);
     }
 
+    public List<Songs> get() {
+        logger.info("Inside SongsService layer!! - [Get Songs]");
+        return songsRepository.findAll();
+    }
+
+    public boolean updateById(String id, SongsDTO songsDTO) {
+        Optional<Songs> findSongById = songsRepository.findById(id);
+        if (findSongById.isPresent()) {
+            Songs _songs = findSongById.get();
+            _songs.setName(songsDTO.getName());
+            _songs.setArtistName(songsDTO.getArtistName());
+            _songs.setCountryArea(songsDTO.getCountryArea());
+            songsRepository.save(_songs);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean deleteAll() {
+        try {
+            songsRepository.deleteAll();
+            return true;
+        } catch (Exception exception) {
+            logger.error(exception.getMessage());
+            return false;
+        }
+    }
+
+    public boolean deleteById(String id) {
+        Optional<Songs> findById = songsRepository.findById(id);
+        if (findById.isPresent()) {
+            songsRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
