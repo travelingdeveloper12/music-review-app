f2b77a53c132da6eaadc534ee5eac5c503a0aa5d
travelingdeveloper12 <dutta_ari@yahoo.in>
2021-04-17 21:09:56 +0100

    Cross Origin && Custom Exception

--- src/main/java/com/musicreview/controller/SongsController.java
+++ src/main/java/com/musicreview/controller/SongsController.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/songs")
 public class SongsController {
@@ -70,6 +70,5 @@ public class SongsController {
             messageResponse.setMessage("No Songs Available");
             return new ResponseEntity<>(messageResponse, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
     }
 }
