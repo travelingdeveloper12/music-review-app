04c06785ac2c2eb25b3ed26c41b1e4fd0071d873
travelingdeveloper12 <dutta_ari@yahoo.in>
2021-04-17 11:01:35 +0100

    MR-First Commit

---
+++ src/main/java/com/musicreview/staging/StagingApplication.java
@@ -0,0 +1,13 @@
+package com.musicreview.staging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicreview/staging/StagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicreview.staging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
